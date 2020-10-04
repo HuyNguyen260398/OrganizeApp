a222820def120db31e8501b4f7eeb2339d5f3832
Huy Nguyen <huynguyen260398@gmail.com>
2020-10-04 11:24:51 +0700

    Component parameters

---
+++ Organize.WASM/Controls/ValidationInputBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Controls
+{
+    public class ValidationInputBase : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<string> ValueChangedCallBack { get; set; }
+
+        [Parameter]
+
+        public string Value { get; set; }
+
+        protected async void HandleInputChanged(ChangeEventArgs eventArgs)
+        {
+            await ValueChangedCallBack.InvokeAsync(eventArgs.Value.ToString());
+        }
+    }
+}
