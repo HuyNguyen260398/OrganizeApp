58fb69daf995e79aab5198065e26bd8fb61c0bd8
Huy Nguyen <huynguyen260398@gmail.com>
2020-10-11 19:08:54 +0700

    Added signup page

---
+++ Organize.WASM/Pages/SignUpBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Pages
+{
+    public class SignUpBase : SignBase
+    {
+
+    }
+}
