a82c2eeaa8863ea16ebbe813c2306184eb36f4ad
Huy Nguyen <huynguyen260398@gmail.com>
2020-11-05 19:35:57 +0700

    Added Js Interop

--- Organize.WASM/Shared/MainLayout.razor.cs
+++ Organize.WASM/Shared/MainLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Organize.Shared.Contracts;
 using System;
 using System.Collections.Generic;
@@ -7,14 +8,58 @@ using System.Threading.Tasks;
 
 namespace Organize.WASM.Shared
 {
-    public partial class MainLayout : LayoutComponentBase
+    public partial class MainLayout : LayoutComponentBase, IDisposable
     {
+        private DotNetObjectReference<MainLayout> _dotNetReference;
+
         [Inject]
         private ICurrentUserService CurrentUserService { get; set; }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+
+        public bool UserShortNavText { get; set; }
 
         protected void SignOut()
         {
 
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var width = await JSRuntime.InvokeAsync<int>("blazorDimension.getWidth");
+            CheckUserShortNavText(width);
+
+            _dotNetReference = DotNetObjectReference.Create(this);
+            await JSRuntime.InvokeVoidAsync("blazorResize.registerReferenceForResizeEvent", _dotNetReference);
+        }
+
+        [JSInvokable]
+        public static void OnResize()
+        {
+
+        }
+
+        [JSInvokable]
+        public void HandleResize(int width, int height)
+        {
+            CheckUserShortNavText(width);
+        }
+
+        private void CheckUserShortNavText(int width)
+        {
+            var oldValue = UserShortNavText;
+            UserShortNavText = width < 700;
+            if (oldValue != UserShortNavText)
+            {
+                StateHasChanged();
+            }
+        }
+
+        public void Dispose()
+        {
+            _dotNetReference?.Dispose();
         }
     }
 }
---
+++ Organize.WASM/wwwroot/js/jsInterop.js
@@ -0,0 +1,21 @@
+﻿window.addEventListener('resize',
+    () => {
+        // Call the static Method OnResize in the Organize assembly
+        console.log("Static resize from js");
+        DotNet.invokeMethodAsync("Organize.WASM", "OnResize");
+    }
+);
+
+window.blazorDimension = {
+    getWidth: () => window.innerWidth
+}
+
+window.blazorResize = {
+    registerReferenceForResizeEvent: (dotnetReference) => {
+        console.log(blazorResize.assignments);
+        window.addEventListener("resize", () => {
+            console.log("HandleResize from js");
+            dotnetReference.invokeMethodAsync("HandleResize", window.innerWidth, window.innerHeight);
+        });
+    }
+}
